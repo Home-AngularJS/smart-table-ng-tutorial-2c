30b9fe85b675986cd17ce527026ee98773ae0b1e
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-10-21 16:07:25 +0300

    #release

--- src/users/default-settings.service.ts
+++ src/users/default-settings.service.ts
@@ -6,10 +6,13 @@ import { Injectable } from '@angular/core'
 export class DefaultSettingsService {
   search = {};
   filter = {
-    balance: [{ operator: 'lt', type: 'number', value: 2000 }]
+    // balance: [{ operator: 'lt', type: 'number', value: 2000 }]
+    // balance: [{ operator: 'gte', type: 'number', value: 3500 }] //TODO  operator: <eq|gt|gte|in|lt|lte|ne|nin>, type: <User.balance: number>, value: <MAX>
+    name: [{ operator: 'lt', type: 'string', value: 'A' }]
   }
   sort = {
-    pointer: 'balance',
+    // pointer: 'balance',
+    pointer: 'name.first',
     direction: 'asc'
   }
   slice = {
--- src/users/users.ts
+++ src/users/users.ts
@@ -4,9 +4,9 @@ interface Name {
 }
 
 export enum Job {
-  'DEV' = 'dev',
-  'QA' = 'qa',
-  'MANAGER' = 'manager'
+  'DEV'='dev',
+  'QA'='qa',
+  'MANAGER'='manager'
 }
 
 export interface User {
@@ -17,14 +17,12 @@ export interface User {
 }
 
 const firstNames = ['Laurent', 'Charlie', 'Elsa', 'Bob', 'Blandine', 'Raymond', 'Jade', 'Athanase', 'Antoine', 'Benjamin', 'Solenne', 'Alice', 'Boris', 'Cedric', 'Camille', 'Isabelle', 'Olivier', 'Nicolas', 'Amaury', 'Odile'];
-
 const lastNames = ['Renard', 'Dupraz', 'Dupont', 'Leponge', 'Robin', 'Blasec', 'Verton', 'Albert', 'Vian', 'Bertin', 'Chevalier', 'Romus', 'Cassare', 'Jourdin', 'Lazarus', 'Blanc', 'Vacon', 'Boulus', 'Giroux', 'Marcelin'];
-
 const jobs = [Job.DEV, Job.QA, Job.MANAGER]
 
 const items: User[] = [];
 
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 100; i++) {
   items.push({
     name: {
       first: firstNames[Math.floor(Math.random() * 20)],
