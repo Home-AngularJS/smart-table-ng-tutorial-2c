b51590395d3d025c896fedfd44eec78f05cd360c
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-10-22 13:09:12 +0300

    #release

--- src/app/core/service/default-settings.service.ts
+++ src/app/core/service/default-settings.service.ts
@@ -5,18 +5,18 @@ import { Injectable } from '@angular/core'
 })
 export class DefaultSettingsService {
   search = {};
+
+  slice = { page: 1, size: 10 };
+
   filter = {
     // balance: [{ operator: 'lt', type: 'number', value: 2000 }]
     // balance: [{ operator: 'gte', type: 'number', value: 3500 }] //TODO  operator: <eq|gt|gte|in|lt|lte|ne|nin>, type: <User.balance: number>, value: <MAX>
     name: [{ operator: 'lt', type: 'string', value: 'A' }]
-  }
-  sort = {
-    // pointer: 'balance',
-    pointer: 'name.first',
-    direction: 'asc'
-  }
-  slice = {
-    page: 1,
-    size: 10
   };
+
+  sort = {
+    pointer: 'name.first', // pointer: 'balance',
+    direction: 'asc'
+  };
+
 }
